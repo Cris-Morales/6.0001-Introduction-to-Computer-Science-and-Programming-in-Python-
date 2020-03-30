3faedacdf1d858fceec730a97dbb929ef1a1bc51
Cris-Morales <62893025+Cris-Morales@users.noreply.github.com>
2020-03-30 09:44:19 -0500

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,14 @@
+"""
+Created Saturday March 28, 2020
+
+@author: Cris Morales
+"""
+#problem set assignment
+
+import math
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+
+print("x**y = ", x**y)
+print("log(x) = ", int(math.log2(x)))
